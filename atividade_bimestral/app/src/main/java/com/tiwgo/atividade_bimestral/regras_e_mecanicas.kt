bc501b199543e7de936b24119295620af1802329
bruno-reload <correia.norte@gmail.com>
2026-09-23 16:31:50 -0300

    start commit

---
+++ atividade_bimestral/app/src/main/java/com/tiwgo/atividade_bimestral/regras_e_mecanicas.kt
@@ -0,0 +1,20 @@
+package com.tiwgo.atividade_bimestral
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class regras_e_mecanicas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_regras_emecanicas)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
